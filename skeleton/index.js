5321c7699a02453ca9d676634d90361cb6626902
Thiyagarajah <Prasath.Thiyagarajah@cic.gc.ca>
2025-10-29 15:52:24 -0400

    addnig files

---
+++ skeleton/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World from Red Hat Developer Hub Template!'));
+app.listen(port, () => console.log(`App running on port ${port}`));
